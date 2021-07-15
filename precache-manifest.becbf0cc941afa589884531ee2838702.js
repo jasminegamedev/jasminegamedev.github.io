2c61cdbabddb7080625386dec8e3b405fb4bc4c7
jasminegamedev <jasminegamedev@users.noreply.github.com>
2021-07-15 05:36:30 +0000

    deploy: 2a14b298b65a2c67ff68fd0fd0b3fba9e7dde1e3

---
+++ precache-manifest.becbf0cc941afa589884531ee2838702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67fa6c0f3a4b94051f307426ec216b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67fb4bb99287bdbe4a79",
+    "url": "/static/css/main.f51755f5.chunk.css"
+  },
+  {
+    "revision": "0e959d9383b37055d943",
+    "url": "/static/js/2.4bdee582.chunk.js"
+  },
+  {
+    "revision": "391bd1161dbcd8c291e67f6f483476eb",
+    "url": "/static/js/2.4bdee582.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67fb4bb99287bdbe4a79",
+    "url": "/static/js/main.3f7bf7fe.chunk.js"
+  },
+  {
+    "revision": "727c0a133cf9012421bd",
+    "url": "/static/js/runtime-main.049e154f.js"
+  }
+]);
