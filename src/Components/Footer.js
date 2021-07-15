ca59bf4af48dd0a6ae9953d15b7700ff2a22039f
jasminegamedev <jasminegamedev@gmail.com>
2021-07-14 22:17:34 -0600

    Copying website and renaming.

---
+++ src/Components/Footer.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss'
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+
+const useStyles = createUseStyles({
+    footer: {
+        fontSize: '24px',
+        margin: 'auto',
+        display: 'table',
+        width: '98%',
+        marginTop: '10px',
+        marginBottom: '10px',
+    },
+    link: {
+        color: 'white',
+        textDecoration: 'none',
+        padding: '5px',
+        '&:hover': {
+            color: 'cyan',
+        },
+    },
+    sourceLink: {
+        color: '#CCC',
+        padding: '5px',
+        '&:hover': {
+            color: 'cyan',
+        },
+    },
+    rightAlign: {
+        float: "right",
+        fontSize: '14px',
+        color: '#BBB',
+    },
+});
+
+export const Footer = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.footer}>
+            <a className={classes.link} href="https://www.linkedin.com/in/j-stephens/" rel="noopener noreferrer" target="_blank">
+                <LinkedInIcon />
+            </a>
+            <a className={classes.link} href="https://twitter.com/JasmineGameDev" rel="noopener noreferrer" target="_blank">
+                <TwitterIcon />
+            </a>
+            <a className={classes.link} href="mailto:jasminegamedev@gmail.com">
+                <EmailIcon />
+            </a>
+            <span className={classes.rightAlign}>
+                {'Website designed and created by Jasmine Stephens -'} 
+                <a className={classes.sourceLink} href="https://github.com/jasminegamedev/jasminegamedev.github.io" rel="noopener noreferrer" target="_blank">
+                    {'View Source'}
+                </a>
+            </span>
+        </div>
+    );
+}
---
+++ src/Pages/Main/HomePage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss'
+import { GameCarousel } from '../../Components/GameCarousel';
+
+const useStyles = createUseStyles({
+    title: {
+        color: 'white',
+        textAlign: 'center',
+        marginBottom: '20px',
+        marginTop: '0px',
+    },
+    body: {
+        margin: '20px',
+        fontSize: '24px',
+        lineSize: '0px',
+    },
+    text: {
+        margin: '20px',
+        fontSize: '20px',
+        textAlign: 'center'
+    },
+    link: {
+        color: 'cyan',
+        fontWeight: 'bold',
+    },
+});
+
+export const HomePage = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.body}>
+            <p className={classes.text}>Hey there, I'm Jasmine! I'm a Game Developer who specializes in Gameplay Programming and UI Programming. I currently work full time as an Enterprise Software Engineer, but I'm always open to hearing about new opportunities! <br/>If you'd like to see some of my past work, I'd encourage you to check out my Game Development <a className={classes.link} href="#/portfolio">Portfolio</a>.</p>
+            <GameCarousel/>
+        </div>
+    );
+}
