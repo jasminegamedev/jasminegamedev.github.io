de7bf6b6a6d5ff544d90568f4cce5047c6b97db1
Jazzrabbit <jasminegamedev@gmail.com>
2022-01-14 12:18:44 -0700

    Updating resume

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -3,37 +3,63 @@ import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
     '@keyframes slideDown': {
-        from: {top: 0},
-        to: {top: '100%'}
+        from: {transform: 'translateY(0)'},
+        to: {transform: 'translateY(100%)'}
     },
     image: {
         width: '100%',
-        height: '101%',
+        height: '100%',
         position: 'fixed',
         zIndex: -100,
         animation: '$slideDown 50s infinite',
         animationTimingFunction: 'linear'
     },
-    '@keyframes slideDown2': {
-        from: {top: '-100%'},
-        to: {top: 0}
-    },
     image2: {
         width: '100%',
-        height: '101%',
+        height: '100%',
         position: 'fixed',
         zIndex: -100,
         animation: '$slideDown2 50s infinite',
         animationTimingFunction: 'linear'
-    }
+    },
+    image3: {
+        width: '100%',
+        height: '100%',
+        position: 'fixed',
+        zIndex: -100,
+        animation: '$slideDown 100s infinite',
+        animationTimingFunction: 'linear'
+    },
+    image4: {
+        width: '100%',
+        height: '100%',
+        position: 'fixed',
+        zIndex: -100,
+        animation: '$slideDown2 100s infinite',
+        animationTimingFunction: 'linear'
+    },
+    background: {
+        background: 'radial-gradient(ellipse at center, #262c68, #000000)',
+        width: '100%',
+        height: '100%',
+        position: 'fixed',
+        zIndex: -105,
+    },
+    '@keyframes slideDown2': {
+        from: {transform: 'translateY(-100%)'},
+        to: {transform: 'translateY(0)'}
+    },
 });
 
 export const Stars = () => {
     const classes = useStyles();
     return (
         <>
-            <img className={classes.image2} src="/content/images/games/tapshot/Stars.png" alt="/content/images/games/tapshot/Stars.png" />
-            <img className={classes.image} src="/content/images/games/tapshot/Stars.png" alt="/content/images/games/tapshot/Stars.png" />
+            <div className={classes.background}></div>
+            <img className={classes.image2} src="/content/images/games/tapshot/Stars1.png" alt="/content/images/games/tapshot/Stars1.png" />
+            <img className={classes.image} src="/content/images/games/tapshot/Stars1.png" alt="/content/images/games/tapshot/Stars1.png" />
+            {/* <img className={classes.image3} src="/content/images/games/tapshot/Stars2.png" alt="/content/images/games/tapshot/Stars2.png" />
+            <img className={classes.image4} src="/content/images/games/tapshot/Stars2.png" alt="/content/images/games/tapshot/Stars2.png" /> */}
         </>
     );
 }
