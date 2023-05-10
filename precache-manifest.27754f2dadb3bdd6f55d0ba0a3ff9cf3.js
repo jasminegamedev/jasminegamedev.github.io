6f72f13cc3cf89f94bc36874900705ef109d88ac
jasminegamedev <jasminegamedev@users.noreply.github.com>
2023-05-10 03:29:15 +0000

    deploy: 74fa7f3bcc135291823b40ea8810532989984bbf

---
+++ precache-manifest.27754f2dadb3bdd6f55d0ba0a3ff9cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fdf968cd386cc93bbcf942bb199191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8364cdbf26205091a6c4",
+    "url": "/static/css/main.f51755f5.chunk.css"
+  },
+  {
+    "revision": "66f591c5b03adbbbfdc9",
+    "url": "/static/js/2.fd4b280f.chunk.js"
+  },
+  {
+    "revision": "391bd1161dbcd8c291e67f6f483476eb",
+    "url": "/static/js/2.fd4b280f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8364cdbf26205091a6c4",
+    "url": "/static/js/main.c57e592f.chunk.js"
+  },
+  {
+    "revision": "727c0a133cf9012421bd",
+    "url": "/static/js/runtime-main.049e154f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07e02aad264f28ee4f7f58add4938642.js"
+  "/precache-manifest.27754f2dadb3bdd6f55d0ba0a3ff9cf3.js"
 );
 
 self.addEventListener('message', (event) => {
