6b59bd49f2ecb367e3b5ffb079ec6e3e2e093edf
Jasmine Stephens <jasminegamedev@gmail.com>
2024-07-04 12:14:43 -0700

    Readding LinkedIn links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss'
 import TwitterIcon from '@material-ui/icons/Twitter';
-// import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import GithubIcon from '@material-ui/icons/GitHub';
 
@@ -40,9 +40,9 @@ export const Footer = () => {
     const classes = useStyles();
     return (
         <div className={classes.footer}>
-            {/* <a className={classes.link} href="https://www.linkedin.com/in/j-stephens/" rel="noopener noreferrer" target="_blank">
+            <a className={classes.link} href="https://www.linkedin.com/in/j-stephens/" rel="noopener noreferrer" target="_blank">
                 <LinkedInIcon />
-            </a> */}
+            </a>
             <a className={classes.link} href="mailto:jasminegamedev@gmail.com">
                 <EmailIcon />
             </a>
